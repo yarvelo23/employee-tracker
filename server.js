3e7a81fcc56de8e64da9821da61d6978258fe8c7
yerson <yerson.a06@gmail.com>
2020-10-28 19:51:44 -0400

    adding mysql connection

--- server.js
+++ server.js
@@ -8,3 +8,20 @@ var mysql = require("mysql");
 
 const util = require("util");
 
+const logo = require('asciiart-logo');
+
+const config = require('./package.json');
+
+// console log to confirm json rendered
+console.log(logo(config).render());
+
+// creating mysql connection dependency
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "AlphaOmega1",
+    database: "Employee_Tracker"
+
+});
+
