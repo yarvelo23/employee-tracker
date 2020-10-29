4216fc054ccc0ea49e262fba00dea72ceb187841
yerson <yerson.a06@gmail.com>
2020-10-28 21:21:58 -0400

    additional modules

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "AlphaOmega1",
+    password: "play2306$",
     database: "Employee_Tracker"
 
 });
